364d3966104a4aa7b6fd42ae467bfdf803ae65ae
r351574nc3@gmail.com <r351574nc3@gmail.com>
2018-01-27 00:17:49 -0700

    Adding setup and documentation

--- README.md
+++ README.md
@@ -4,10 +4,4 @@ Steem bot docker image for using git to post to steem
 
 ## Usage
 
-`docker run --rm -p 3000:3000 my-repo/my-steem-bot:latest`
-
-### Mac OS Users
-
-can run the following after
-
-`open http://localhost:3000`
+curl https://github.com/r351574nc3/docker-git-steem-bot/blog/master/setup.sh
---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -rf $HOME/.steemgit $HOME/.steemgitrc $3
+mkdir -p $HOME/.steemgit
+
+cat > $HOME/.steemgitrc <<EOF
+GIT_USER=$1
+GIT_TOKEN=$2
+STEEMIT_GIT_PROJECT=$3
+alias steemgit='docker run --privileged --rm -v $PWD:/work -v $HOME/.steemgit:/steem --entrypoint=git r351574nc3/git-steem-bot:latest'
+alias steemgit_setup='docker run --privileged --rm -v $PWD:/work -v $HOME/.steemgit:/steem r351574nc3/git-steem-bot:latest /opt/bin/setup.sh github.com $GIT_USER $GIT_TOKEN'
+
+export GIT_USER GIT_TOKEN STEEMIT_GIT_PROJECT
+EOF
+
+cat $HOME/.steemgitrc
