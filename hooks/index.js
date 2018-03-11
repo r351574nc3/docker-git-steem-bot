957b57f9ca3263a0f81a3553f3feefc43d86c090
r351574nc3@gmail.com <r351574nc3@gmail.com>
2018-03-11 08:15:23 -0700

    Using post-update hook, so objects are completely added before creating the post. Update hooks to use notes to define metadata and comments options

--- hooks/index.js
+++ hooks/index.js
@@ -1,11 +1,87 @@
 const Promise = require('bluebird')
 const steem = Promise.promisifyAll(require('steem'))
 const fs = Promise.promisifyAll(require('fs'))
+const shell = require('shelljs');
 
 
+const defaults = {
+    comment_options: {
+        "extensions":[],
+        "operations":[
+            [
+                "comment_options",
+                {
+                    "author":"",
+                    "permlink":"",
+                    "max_accepted_payout":"1000000.000 SBD",
+                    "percent_steem_dollars": "10000",
+                    "allow_votes": true,
+                    "allow_curation_rewards": true,
+                    "extensions":[]
+                }
+            ]
+        ]
+    },
+    metadata: { tags: [], app: 'r351574nc3/docker-git-steem-bot' },
+    beneficiaries: []
+}
 
 function load_post() {
     return fs.readFileAsync(0, 'utf8')
+}
+
+function load_beneficiaries(repo) {
+    if (shell.exec(`git -C ${repo} notes --ref=beneficiaries list`) == '') {
+        return defaults.beneficiaries
+    }
+    let ref = shell.exec(`git -C ${repo} notes --ref=beneficiaries list`).exec("cut -f 2 -d ' '")
+    let data = shell.exec(`git -C ${repo} notes --ref=beneficiaries show ${ref}`)
+
+    
+    if (data != '') {
+        let retval = [
+            [
+                0,
+                {
+                    beneficiaries: [
+                    ]
+                }
+            ]
+        ]
+        JSON.parse(data).beneficiaries.split(",").forEach((kvpair) => {
+            let { account, weight } = kvpair.split(":");
+            retval[0][1].beneficiaries.push({ account: account, weight: weight })            
+        })
+        return retval
+    }
+    return defaults.beneficiaries
+    
+}
+
+function load_metadata(repo) {
+    if (shell.exec(`git -C ${repo} notes --ref=metadata list`) == '') {
+        return defaults.metadata
+    }
+    let ref = shell.exec(`git -C ${repo} notes --ref=metadata list`).exec("cut -f 2 -d ' '")
+    let data = shell.exec(`git -C ${repo} notes --ref=metadata show ${ref}`)
+    if (data != '') {
+        let retval = JSON.parse(data)
+        retval.app = 'r351574nc3/docker-git-steem-bot'
+    }
+    return defaults.metadata
+}
+
+function load_comment_options(repo) {
+    if (shell.exec(`git -C ${repo} notes --ref=comment_options list`) == '') {
+        return defaults.comment_options
+    }
+
+    let ref = shell.exec(`git -C ${repo} notes --ref=comment_options list`).exec("cut -f 2 -d ' '")
+    let retval = shell.exec(`git -C ${repo} notes --ref=comment_options show ${ref}`)
+    if (retval != '') {
+        return JSON.parse(retval)
+    }
+    return defaults.comment_options
 }
 
 function main() {
@@ -13,9 +89,16 @@ function main() {
     let wif = process.env.STEEM_WIF || "Not set"
     let permlink = process.argv[2]
     let title = process.argv[3]
-/*
+    let repo  = process.argv[4]
+
+    let metadata = load_metadata(repo);
+    let comment_options = load_comment_options(repo);
+    comment_options.operations[0][1].author = user
+    comment_options.operations[0][1].permlink = permlink
+    comment_options.operations[0][1].extensions = load_beneficiaries(repo);
+
     load_post().then((body) => {
-        var permlink = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
+        console.log("Permlink ", permlink)
         return steem.broadcast.commentAsync(
             wif,
             '', // Leave parent author empty
@@ -24,12 +107,23 @@ function main() {
             permlink + '-post', // Permlink
             title, // Title
             body, // Body
-            { tags: [], app: 'r351574nc3/docker-git-steem-bot' }
+            metadata
         )
-    }).then((results) => {
-        console.log(results)
-    }) 
-*/
+    })
+    .then((results) => {
+        steem.broadcast.send(wif, comment_options, function(err, results) {
+            if (err) {
+                console.log("Unable to set comment options ", JSON.stringify(err));
+                return
+            }
+            console.log("Results ", results)
+        });
+    })
+        .catch((err) => {
+            console.log(err)
+        })
+    
+    console.log("Exiting")
 }
 
-main()
+return main()
